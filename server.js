b9995d09ea40566d1ba6d55e5435f2672b71f847
Rushi <86092539+issarush@users.noreply.github.com>
2023-04-01 17:43:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express()
 const port = 3000
 
 //Using Express and Node to create a server on port 3000
-//Total packages used are 3: express, ejs, and nodemon
-//Serving static files from the public folder and using EJS as the view engine
+//Total packages used are 2: express and nodemon
+//Serving static files from the public folder
 
 app.use(express.static("./public"))
-
-app.set("view engine", "ejs")
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
