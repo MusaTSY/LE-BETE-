077382f5dfbe352565f8acc124c2d195f2fdd5b6
Rushi <86092539+issarush@users.noreply.github.com>
2023-04-01 17:59:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+//Using Node's express framework to setup a connection via port 3000
+//Total packages used are 2: express and nodemon
+
 const express = require('express');
 const app = express()
 const port = 3000
 
-//Using Express and Node to create a server on port 3000
-//Total packages used are 2: express and nodemon
 //Serving static files from the public folder
-
 app.use(express.static("./public"))
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
